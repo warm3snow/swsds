12a9a1fb4450555913dec5ab16e8c36a3323882c
warm3snow <xueyanghan@hotmail.com>
2016-11-30 06:49:40 -0800

    first version swsds

---
+++ swsds.go
@@ -0,0 +1,233 @@
+/*encapsalute sansec api in golang*/
+package swsds
+
+//#cgo LDFLAGS: -L ./ -lswsds
+//#include "./swsds.h"
+import "C"
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+	"unsafe"
+)
+
+type swcsp struct {
+	hDev unsafe.Pointer
+}
+
+func NewSwcsp() *swcsp {
+	var hDeviceHandle C.SGD_HANDLE
+	rv := C.SDF_OpenDevice(&hDeviceHandle)
+	if rv != C.SDR_OK {
+		log.Panicf("OpenDevice fail, rv=%#X\n", rv)
+	}
+	hDevHdl := unsafe.Pointer(hDeviceHandle)
+	return &swcsp{hDev: hDevHdl}
+}
+
+func (t *swcsp) CloseDevice() {
+	hDeviceHandle := C.SGD_HANDLE(t.hDev)
+	rv := C.SDF_CloseDevice(hDeviceHandle)
+	if rv != C.SDR_OK {
+		log.Panicf("CloseDevice fail, rv =%#X\n", rv)
+	}
+}
+
+func (t *swcsp) OpenSession() (unsafe.Pointer, error) {
+	var hSessionHandle C.SGD_HANDLE
+	rv := C.SDF_OpenSession(t.hDev, &hSessionHandle)
+	if rv != C.SDR_OK {
+		return nil, fmt.Errorf("OpenSession fail, rv=%#X\n", rv)
+	}
+	return unsafe.Pointer(hSessionHandle), nil
+}
+
+func (t *swcsp) CloseSession(ssHandle unsafe.Pointer) {
+	hSessionHandle := C.SGD_HANDLE(ssHandle)
+	rv := C.SDF_CloseSession(hSessionHandle)
+	if rv != C.SDR_OK {
+		log.Panicf("OpenSession fail, rv=%#X\n", rv)
+	}
+}
+
+func (t *swcsp) SM2_GenKeyPair(ssHandle unsafe.Pointer) (*ecdsa.PrivateKey, error) {
+	var rv C.SGD_RV
+	var keyLen C.SGD_UINT32 = 256
+	var pubKey C.struct_ECCrefPublicKey_st
+	var priKey C.struct_ECCrefPrivateKey_st
+	hSessionHandle := C.SGD_HANDLE(ssHandle)
+
+	rv = C.SDF_GenerateKeyPair_ECC(hSessionHandle, C.SGD_SM2_3, keyLen, &pubKey, &priKey)
+	if rv != C.SDR_OK {
+		return nil, fmt.Errorf("GenKeyPair fails, rv = %#X\n", rv)
+	}
+
+	priv := new(ecdsa.PrivateKey)
+
+	priv.D = new(big.Int).SetBytes(UcharArrToByteArr(priKey.D[:]))
+	priv.PublicKey.X = new(big.Int).SetBytes(UcharArrToByteArr(pubKey.x[:]))
+	priv.PublicKey.Y = new(big.Int).SetBytes(UcharArrToByteArr(pubKey.y[:]))
+
+	return priv, nil
+}
+
+func (t *swcsp) SM2_Sign(ssHandle unsafe.Pointer, priv *ecdsa.PrivateKey, hash []byte) (r, s *big.Int, err error) {
+	var rv C.SGD_RV
+	var priKey C.struct_ECCrefPrivateKey_st
+	var signature C.struct_ECCSignature_st
+
+	//inData length must be 32bytes, see ByteArrToSGD_UCHARArr
+	inData := ByteArrToSGD_UCHARArr(hash)
+	var inLen C.SGD_UINT32 = 32
+	hSessionHandle := C.SGD_HANDLE(ssHandle)
+	/*
+		rv = C.SDF_GenerateRandom(hSessionHandle, inLen, &inData[0])
+		if rv != C.SDR_OK {
+			fmt.Printf("gen random fail, rv = %x\n", rv)
+		}
+	*/
+	priKey.bits = 256
+	copy(priKey.D[:], ByteArrToUcharArr(priv.D.Bytes()))
+
+	rv = C.SDF_ExternalSign_ECC(hSessionHandle, C.SGD_SM2_1, &priKey, &inData[0], inLen, &signature)
+	if rv != C.SDR_OK {
+		return nil, nil, fmt.Errorf("SM2_ExternalSign fail, rv = %#X\n", rv)
+	}
+
+	r, s = new(big.Int), new(big.Int)
+	r.SetBytes(UcharArrToByteArr(signature.r[:]))
+	s.SetBytes(UcharArrToByteArr(signature.s[:]))
+
+	return r, s, nil
+}
+
+func (t *swcsp) SM2_Verify(ssHandle unsafe.Pointer, pub *ecdsa.PublicKey, hash []byte, r, s *big.Int) (bool, error) {
+	var rv C.SGD_RV
+	var pubKey C.struct_ECCrefPublicKey_st
+	var signature C.struct_ECCSignature_st
+	var inLen C.SGD_UINT32
+	hSessionHandle := C.SGD_HANDLE(ssHandle)
+
+	//inData length must be 32bytes, see ByteArrToSGD_UCHARArr
+	inData := ByteArrToSGD_UCHARArr(hash)
+	inLen = 32
+	copy(pubKey.x[:], ByteArrToUcharArr(pub.X.Bytes()))
+	copy(pubKey.y[:], ByteArrToUcharArr(pub.Y.Bytes()))
+	pubKey.bits = 256
+	copy(signature.r[:], ByteArrToUcharArr(r.Bytes()))
+	copy(signature.s[:], ByteArrToUcharArr(s.Bytes()))
+
+	rv = C.SDF_ExternalVerify_ECC(hSessionHandle, C.SGD_SM2_1, &pubKey, &inData[0], inLen, &signature)
+
+	fmt.Println()
+	if rv != C.SDR_OK {
+		return false, fmt.Errorf("SM2_Verify fails, rv = %#X\n", rv)
+	}
+
+	return true, nil
+}
+
+func (t *swcsp) SM3_Hash(ssHandle unsafe.Pointer, msg []byte) ([]byte, error) {
+	hSessionHandle := C.SGD_HANDLE(ssHandle)
+	var rv C.SGD_RV
+	var outData [128]C.SGD_UCHAR
+	var inLen, outLen C.SGD_UINT32
+
+	inData := ByteArrToSGD_UCHARArr(msg)
+	inLen = C.SGD_UINT32(len(msg))
+
+	rv = C.SDF_HashInit(hSessionHandle, C.SGD_SM3, nil, nil, 0)
+	if rv == C.SDR_OK {
+		rv = C.SDF_HashUpdate(hSessionHandle, &inData[0], inLen)
+		if rv == C.SDR_OK {
+			C.SDF_HashFinal(hSessionHandle, &outData[0], &outLen)
+			if rv != C.SDR_OK {
+				return nil, fmt.Errorf("SM3_HashFinal fail, rv=%#X\n", rv)
+			}
+		}
+	} else {
+		return nil, fmt.Errorf("SM3_Hash fail, rv=%#X\n", rv)
+	}
+	return SGD_UCHARArrToByteArr(outData[:outLen]), nil
+}
+
+func (t *swcsp) SM3_HashInit(handle unsafe.Pointer) error {
+	hSessionHandle := C.SGD_HANDLE(handle)
+	var rv C.SGD_RV
+
+	rv = C.SDF_HashInit(hSessionHandle, C.SGD_SM3, nil, nil, 0)
+	if rv == C.SDR_OK {
+		return fmt.Errorf("SM3_HashFinal fail, rv=%#X\n", rv)
+	}
+	return nil
+}
+func (t *swcsp) SM3_HashUpdate(ssHandle unsafe.Pointer, msg []byte) error {
+	hSessionHandle := C.SGD_HANDLE(ssHandle)
+	var rv C.SGD_RV
+	var inLen C.SGD_UINT32
+
+	inData := ByteArrToSGD_UCHARArr(msg)
+	inLen = C.SGD_UINT32(len(msg))
+
+	rv = C.SDF_HashUpdate(hSessionHandle, &inData[0], inLen)
+	if rv == C.SDR_OK {
+		return fmt.Errorf("SM3_HashFinal fail, rv=%#X\n", rv)
+	}
+	return nil
+}
+func (t *swcsp) SM3_HashFinal(ssHandle unsafe.Pointer) ([]byte, error) {
+	hSessionHandle := C.SGD_HANDLE(ssHandle)
+	var rv C.SGD_RV
+	var outData [128]C.SGD_UCHAR
+	var outLen C.SGD_UINT32
+
+	rv = C.SDF_HashFinal(hSessionHandle, &outData[0], &outLen)
+	if rv != C.SDR_OK {
+		return nil, fmt.Errorf("SM3_HashFinal fail, rv=%#X\n", rv)
+	}
+	return SGD_UCHARArrToByteArr(outData[:outLen]), nil
+}
+
+func (t *swcsp) SM3_hmac(handle unsafe.Pointer, key, msg []byte) ([]byte, error) {
+	//TODO
+}
+
+/* util funcs */
+func UcharArrToByteArr(buf []C.uchar) []byte {
+	var ret []byte
+	for i := 0; i < len(buf); i++ {
+		ret = append(ret, byte(buf[i]))
+	}
+	return ret
+}
+func ByteArrToUcharArr(buf []byte) []C.uchar {
+	var ret []C.uchar
+	for i := 0; i < 32; i++ {
+		if i < len(buf) {
+			ret = append(ret, C.uchar(buf[i]))
+		} else {
+			ret = append(ret, C.uchar([]byte("0")[0]))
+		}
+	}
+	return ret
+}
+func ByteArrToSGD_UCHARArr(buf []byte) []C.SGD_UCHAR {
+	var ret []C.SGD_UCHAR
+	for i := 0; i < 32; i++ {
+		if i < len(buf) {
+			ret = append(ret, C.SGD_UCHAR(buf[i]))
+		} else {
+			ret = append(ret, C.SGD_UCHAR([]byte("0")[0]))
+		}
+	}
+	return ret
+}
+func SGD_UCHARArrToByteArr(buf []C.SGD_UCHAR) []byte {
+	var ret []byte
+	for i := 0; i < len(buf); i++ {
+		ret = append(ret, byte(buf[i]))
+	}
+	return ret
+}
---
+++ swsds_test.go
@@ -0,0 +1,161 @@
+package swsds
+
+import "testing"
+
+var csp *swcsp = NewSwcsp()
+
+func Test_OpenSession(t *testing.T) {
+	hSess, _ := csp.OpenSession()
+	defer csp.CloseSession(hSess)
+}
+
+func Test_SM2_GenKeyPair(t *testing.T) {
+	hSess, _ := csp.OpenSession()
+	defer csp.CloseSession(hSess)
+
+	_, err := csp.SM2_GenKeyPair(hSess)
+	if err != nil {
+		t.Error("GenKeyPair fail")
+	}
+}
+
+func Test_SM2_SignAndVerify(t *testing.T) {
+	hSess, _ := csp.OpenSession()
+	defer csp.CloseSession(hSess)
+
+	priKey, err := csp.SM2_GenKeyPair(hSess)
+	if err != nil {
+		t.Error("GenKeyPair fail")
+	}
+	//test the priKey by Sign_Verify
+	msg := []byte("sansec")
+	r, s, err := csp.SM2_Sign(hSess, priKey, msg)
+	if err != nil {
+		t.Error("Sign err")
+	}
+	ok, _ := csp.SM2_Verify(hSess, &priKey.PublicKey, msg, r, s)
+	if !ok {
+		t.Error("Verify fail")
+	}
+}
+
+/*
+func Test_SM4_EncAndDec(t *testing.T) {
+		    hSess := csp.OpenSession()
+			msg := []byte("sansec")
+
+			cpr := csp.SM4_Encrypt(hSess, msg)
+			pln := csp.SM4_Decrypt(hSess, cpr)
+
+			if len(pln) != len(msg) {
+				t.Error("SM4 err")
+			}
+			for i := 0; i < len(pln); i++ {
+				if msg[i] != pln[i] {
+					t.Error("SM4 decrypt err")
+				}
+			}
+}
+*/
+func Test_SM3_Hash(t *testing.T) {
+	var msg []byte = []byte("sansec")
+
+	hSess, _ := csp.OpenSession()
+	defer csp.CloseSession(hSess)
+
+	res1, _ := csp.SM3_Hash(hSess, msg)
+
+	csp.SM3_HashInit(hSess)
+	csp.SM3_HashUpdate(hSess, msg)
+	res2, _ := csp.SM3_HashFinal(hSess)
+
+	if len(res1) != len(res2) {
+		t.Error("wrong length")
+	}
+
+	for i := 0; i < len(res1) && i < len(res2); i++ {
+		if res1[i] != res2[i] {
+			t.Error("hash err")
+		}
+	}
+}
+
+/*
+func Test_SM3_hmac(t *testing.T) {
+	hSess := csp.OpenSession()
+	defer csp.CloseSession(hSess)
+	var key []byte = []byte("key")
+	var msg1 []byte = []byte("sansec")
+	var msg2 []byte = []byte("swsds")
+
+	hmac1, _ := csp.SM3_hmac(hSess, key, msg1)
+	hmac2, _ := csp.SM3_hmac(hSess, key, msg2)
+	if len(hmac1) != len(hmac2) {
+		t.Error("hmac length err")
+	}
+	var same bool = true
+	for i := 0; i < len(hmac1); i++ {
+		if hmac1[i] != hmac2[i] {
+			same = false
+		}
+	}
+	if same {
+		t.Error("hmac err")
+	}
+}
+
+func Test_SM2_multAdd(t *testing.T) {
+	hSess := csp.OpenSession()
+	defer csp.CloseSession(hSess)
+
+	var err error
+	ePri, err := csp.SM2_GenKeyPair(hSess)
+	aPri, err := csp.SM2_GenKeyPair(hSess)
+	bPri, err := csp.SM2_GenKeyPair(hSess)
+
+	aPub := aPri.PublicKey
+	bPub := bPri.PublicKey
+
+	cPub, err := csp.SM2_multAdd(hSess, 0, ePri, &aPub, &bPub)
+	if err != nil {
+		t.Error("MultAdd fail")
+	}
+	_ = cPub
+}
+
+func Test_SM2_modMultAdd(t *testing.T) {
+	hSess := csp.OpenSession()
+	defer csp.CloseSession(hSess)
+
+	var err error
+	kPri, err := csp.SM2_GenKeyPair(hSess)
+	aPri, err := csp.SM2_GenKeyPair(hSess)
+	bPri, err := csp.SM2_GenKeyPair(hSess)
+
+	cPri, err := csp.SM2_modMultAdd(hSess, kPri, aPri, bPri)
+	if err != nil {
+		t.Error("ModMultAdd fail")
+	}
+	_ = cPri
+
+}
+
+func Test_SM4_crypt_ecb(t *testing.T) {
+	key := []byte("123456123456")
+	msg := []byte("sansec")
+	cpr := csp.SM4_crypt_ecb(key, msg)
+	if cpr == nil {
+		t.Error("SM4_crypt_ecb fail")
+	}
+}
+*/
+/*
+msg:  hello world, hello world
+hash:  [195 67 246 122 52 35 150 30 157 147 203 31 13 106 170 112 64 231 199 218 191 102 189 69 92 108 65 127 153 250 104 42]
+D:  23975483598876884298529059711121659238225508025131081770608950953671194982528502537563870996636402457202068344087032
+X:  36867246183724785534670931138273922488717281608782831811305615183801315860173673093595414044290690635411499643182489
+Y:  3579336333986994787125356962138736758359030708665150999359108283664854301272477348861368004805555546749552833445695
+r:  21779111779685509038872927360013300214564093712045271632247014149709540297545159959367291323906847926743829456856698
+s:  7696660709777259717489567807873733678903414136728780938898212388762710735375873038673985898514281428606944926001041
+Verify Result:  true
+*/
